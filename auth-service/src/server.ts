9dc88a0ca982230f10e69080dcd79ab7bf54265b
olegkrasilev <olegkrasilev@gmail.com>
2025-05-21 11:50:11 +0300

    feat: helmet

--- auth-service/src/server.ts
+++ auth-service/src/server.ts
@@ -4,9 +4,11 @@ import { responseLoggerMiddleware } from "./middleware/logger/responseLoggerMidd
 import { errorLoggerMiddleware } from "./middleware/logger/errorLoggerMiddleware";
 import { errorHandlerMiddleware } from "./middleware/error";
 import config from "./config/env";
+import helmet from "helmet";
 
 export const app = express();
 
+app.use(helmet());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(requestLoggerMiddleware);
